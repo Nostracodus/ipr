419abff5a7512766aae4a16137f9bd760cf3bc06
Cody Miller <codym@microsoft.com>
2021-02-01 14:06:47 -0800

    style: nounify some verbal node forms

--- src/traversal.cxx
+++ src/traversal.cxx
@@ -319,7 +319,7 @@ ipr::Visitor::visit(const Delete& e)
 }
 
 void
-ipr::Visitor::visit(const Demote& e)
+ipr::Visitor::visit(const Demotion& e)
 {
    visit(as<Expr>(e));
 }
@@ -367,7 +367,7 @@ ipr::Visitor::visit(const Unary_minus& e)
 }
 
 void
-ipr::Visitor::visit(const Materialize& e)
+ipr::Visitor::visit(const Materialization& e)
 {
    visit(as<Expr>(e));
 }
@@ -403,7 +403,7 @@ ipr::Visitor::visit(const Pre_increment& e)
 }
 
 void
-ipr::Visitor::visit(const Promote& e)
+ipr::Visitor::visit(const Promotion& e)
 {
    visit(as<Expr>(e));
 }
@@ -714,7 +714,7 @@ void ipr::Visitor::visit(const Binary_fold& e)
 }
 
 void
-ipr::Visitor::visit(const Coerce& e)
+ipr::Visitor::visit(const Coercion& e)
 {
    visit(as<Expr>(e));
 }
